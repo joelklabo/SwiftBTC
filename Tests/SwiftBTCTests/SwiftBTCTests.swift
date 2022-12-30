ac35ada88b32724a8e8410dacbbbe867abdfd877
Joel Klabo <joelklabo@gmail.com>
2022-12-29 16:42:08 -0800

    Add BigInt Dependency

--- Tests/SwiftBTCTests/SwiftBTCTests.swift
+++ Tests/SwiftBTCTests/SwiftBTCTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import SwiftBTC
 
 final class SwiftBTCTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftBTC().text, "Hello, World!")
-    }
+
 }
